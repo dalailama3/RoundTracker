ba14a2c5b65e1d807968a426d115c873990b0ef7
John Clark <johngclark36@gmail.com>
2016-05-26 18:09:48 -0400

    Display table in RoundShow template, add method on round model to calculate round score

--- app/assets/javascripts/models/round.js
+++ app/assets/javascripts/models/round.js
@@ -1,3 +1,13 @@
 window.RoundTracker.Models.Round = Backbone.Model.extend({
-  urlRoot: "api/rounds"
+  urlRoot: "api/rounds",
+
+  totalScore: function () {
+    var self = this;
+    var total = 0;
+    var hash = self.get("score_hash");
+    for (var hole in hash) {
+      total += parseInt(hash[hole]);
+    }
+    return total;
+  }
 });
--- app/assets/javascripts/views/rounds/show.js
+++ app/assets/javascripts/views/rounds/show.js
@@ -1,11 +1,14 @@
 window.RoundTracker.Views.RoundShow = Backbone.View.extend({
   template: JST["rounds/show"],
+  initialize: function (options) {
+    this.listenTo(this.model, "sync", this.render)
+  },
 
   render: function () {
     var content = this.template({round: this.model});
     this.$el.html(content);
 
-    
+
 
     return this;
   }
