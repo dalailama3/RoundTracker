734765d61eff0779241aba6f5c1e01625bd4ae1a
John Clark <johngclark36@gmail.com>
2016-06-04 01:14:10 -0400

    Add additional information table info for round on show template

--- app/assets/javascripts/models/round.js
+++ app/assets/javascripts/models/round.js
@@ -1,3 +1,14 @@
 window.RoundTracker.Models.Round = Backbone.Model.extend({
   urlRoot: "api/rounds",
+  fairwaysCount: function () {
+    var self = this;
+    var hash = self.get("fairways_hash");
+    var count = 0;
+    for (var key in hash) {
+      if (hash[key] === "Y") {
+        count += 1;
+      }
+    }
+    return count;
+  }
 });
