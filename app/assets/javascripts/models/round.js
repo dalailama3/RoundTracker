dd3eb59c7e9c9c7034a29f3a2d643c8a5e137282
John Clark <johngclark36@gmail.com>
2016-06-05 22:26:38 -0400

    Move totalPutts logic into Round Backbone Model definition

--- app/assets/javascripts/models/round.js
+++ app/assets/javascripts/models/round.js
@@ -10,5 +10,15 @@ window.RoundTracker.Models.Round = Backbone.Model.extend({
       }
     }
     return count;
+  },
+
+  totalPutts: function () {
+    var self = this;
+    var puttsTotal = 0;
+    var puttsHash = self.get("putts_hash");
+    for (var key in puttsHash) {
+      puttsTotal += isNaN(parseInt(puttsHash[key])) ? 0 : parseInt(puttsHash[key]);
+    }
+
   }
 });
