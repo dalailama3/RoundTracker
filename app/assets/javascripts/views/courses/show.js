1ff24091cf70052aa78d48c0831a01948aea95b2
John Clark <johngclark36@gmail.com>
2016-06-07 01:04:27 -0400

    Write CourseShow#deleteCourse which destroys a course upon clicking the delete button

--- app/assets/javascripts/views/courses/show.js
+++ app/assets/javascripts/views/courses/show.js
@@ -3,6 +3,18 @@ window.RoundTracker.Views.CourseShow = Backbone.View.extend({
   initialize: function (options) {
     this.listenTo(this.model, "sync", this.render)
   },
+  events: {
+      "click button.delete": "deleteCourse"
+  },
+
+  deleteCourse: function (e) {
+    this.model.destroy({
+      success: function () {
+        Backbone.history.navigate("#courses", {trigger: true});
+      }
+    });
+  },
+
   render: function () {
     var content = this.template({course: this.model});
     this.$el.html(content);
