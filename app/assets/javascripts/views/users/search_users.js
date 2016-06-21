4711a76bda65ae0236c50e5928f0c19b4a18a334
John Clark <johngclark36@gmail.com>
2016-06-21 12:41:03 -0400

    Add event to SearchUsers View, keyup in text input field triggers startSearch which takes input value and checks it against email strings users collection

--- app/assets/javascripts/views/users/search_users.js
+++ app/assets/javascripts/views/users/search_users.js
@@ -1,9 +1,38 @@
 window.RoundTracker.Views.SearchUsers = Backbone.View.extend({
   template: JST["users/search"],
+  events: {
+    "keyup #search_users": "startSearch"
+  },
   render: function () {
     var content = this.template({users: this.collection});
     this.$el.html(content);
 
     return this;
+  },
+
+  startSearch: function (e) {
+    e.preventDefault();
+    var val = $("#search_users").val();
+    val = val.toLowerCase();
+
+    var models = this.collection.models;
+    var arr = [];
+    _.each(models, function (model) {
+      arr.push(model.attributes.email);
+    });
+
+    var result = [];
+    _.each(arr, function (str) {
+      if (str.startsWith(val)) {
+        result.push('<li>' + str + '</li>');
+      }
+    });
+
+    if (val.length > 0) {
+      $("ul.users").html(result.join(''));
+    } else {
+      $("ul.users").empty();
+    }
+
   }
 });
