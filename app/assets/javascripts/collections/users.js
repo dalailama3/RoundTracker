d7c98370b1f6bb6beee6b906bf1ce7ea6c90913c
John Clark <johngclark36@gmail.com>
2016-06-23 16:45:19 -0400

    Add User Backbone model

--- app/assets/javascripts/collections/users.js
+++ app/assets/javascripts/collections/users.js
@@ -1,5 +1,6 @@
 window.RoundTracker.Collections.Users = Backbone.Collection.extend({
-  url: "api/users"
+  url: "api/users",
+  model: RoundTracker.Models.User
 });
 
 window.RoundTracker.Collections.users = new RoundTracker.Collections.Users();
---
+++ app/assets/javascripts/models/user.js
@@ -0,0 +1,3 @@
+window.RoundTracker.Models.User = Backbone.Model.extend({
+  urlRoot: "api/users"
+});
