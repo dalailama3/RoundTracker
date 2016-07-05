769cccec87f8abdae0b973ae4c4d747ea18e1d75
John Clark <johngclark36@gmail.com>
2016-07-04 20:23:04 -0400

    Edit UsersController#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      log_in!
+      log_in!(@user)
       redirect_to "#rounds"
     else
       render :new
