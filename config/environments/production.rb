ec770c3bb711acdd71ebd91bb3a3e2816c83a328
John Clark <johngclark36@gmail.com>
2016-07-08 16:24:43 -0400

    Change smtp settings again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,13 +64,13 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { :host => 'www.roundtracker.io' }
+  config.action_mailer.default_url_options = { :host => 'roundtracker.herokuapp.com' }
   config.action_mailer.smtp_settings = {
   :address              => 'smtp.gmail.com',
-  :port                 => 587,
+  :port                 => '465',
   :domain               => 'gmail.com',
-  :user_name            => 'EMAIL_ADDRESS@gmail.com',
-  :password             => 'pass',
+  :user_name            => 'johngclark36@gmail.com',
+  :password             => 'newBeginning26',
   :authentication       => :plain,
   :enable_starttls_auto => true,
   :openssl_verify_mode  => 'none'
