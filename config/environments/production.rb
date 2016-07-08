9198cb0feff7ea3309b48c353ad350e97d8cf9f1
John Clark <johngclark36@gmail.com>
2016-07-08 05:01:28 -0400

    Add settings smtp in config/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,17 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { :host => 'www.roundtracker.io' }
+  config.action_mailer.smtp_settings = {
+  :address              => 'smtp.gmail.com',
+  :port                 => 587,
+  :domain               => 'gmail.com',
+  :user_name            => 'EMAIL_ADDRESS@gmail.com',
+  :password             => 'pass',
+  :authentication       => :plain,
+  :enable_starttls_auto => true,
+  :openssl_verify_mode  => 'none'
+}
+
 
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
